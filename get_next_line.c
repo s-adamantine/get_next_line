75c56fb2fdda3a0d40c6bc7fe0358f09187aa08e
sadamant <sharfy.adamantine@gmail.com>
2017-11-10 12:52:10 +0100

    fix return values

--- get_next_line.c
+++ get_next_line.c
@@ -19,7 +19,6 @@ void   		expand_line(char **line)
 
 	expanded = ft_strnew(ft_strlen(*line) + BUFF_SIZE); //strnew vs memalloc hmm one possible extra byte
 	ft_strcpy(expanded, *line);
-	//probably need to free something here.
 	*line = expanded;
 }
 
@@ -29,19 +28,19 @@ void   		expand_line(char **line)
 char	*transfer(char **line, char *buf)
 {
 	int		i;
-	char	*add;
+	char	*substring;
 	char	*new_buf;
 
 	i = 0;
-	add = ft_strnew(BUFF_SIZE);
+	substring = ft_strnew(BUFF_SIZE);
 	new_buf = ft_strnew(BUFF_SIZE);
 	while (*buf && *buf != '\n')
 	{
-		add[i++] = *buf++;
+		substring[i++] = *buf++;
 	}
-	ft_strcat(*line, add);
+	ft_strcat(*line, substring);
 	if (ft_strchr(buf, '\n'))
-		ft_strcpy(new_buf, ft_strchr(buf, '\n') + 1); //copy to the temp everything from buf after the first \n.
+		ft_strcpy(new_buf, ft_strchr(buf, '\n') + 1); //copy to the temp everything from buf after the first \n. FUCKING UP HERE
 	else
 		ft_bzero(new_buf, BUFF_SIZE);
 	return (new_buf);
@@ -52,22 +51,36 @@ int		get_next_line(const int fd, char **line)
 	static char	*buf;
 
 	*line = ft_memalloc(BUFF_SIZE);
+	if (fd < 0)
+		return (-1);
 	if (buf)
 	{
-		buf = transfer(line, buf);
-		if (!*buf)
-			expand_line(line);
-		else
+		if (ft_strchr(buf, '\n'))
+		{
+			buf = transfer(line, buf);
 			return (1);
+		}
+		else
+		{
+			buf = transfer(line, buf);
+			expand_line(line);
+		}
 	}
 	buf = ft_strnew(BUFF_SIZE);
-	while (read(fd, buf, BUFF_SIZE) > 0)
+	while (read(fd, buf, BUFF_SIZE) > 0) //while you can read
 	{
-		buf = transfer(line, buf);
-		if (!*buf)
-			expand_line(line);
-		else
+		if (ft_strchr(buf, '\n'))
+		{
+			buf = transfer(line, buf);
 			return (1);
+		}
+		else
+		{
+			buf = transfer(line, buf);
+			expand_line(line);
+		}
 	}
+	if (**line)
+		return (1);
 	return (0);
 }
